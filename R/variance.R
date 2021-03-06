75e611738296a30596162d8f6d82f8784f2a534d
SoulStealer07 <jhi131@my.utsa.edu>
2021-03-05 19:48:37 -0600

    3rd Commit

--- R/variance.R
+++ R/variance.R
@@ -1,4 +1,18 @@
+#' Compute Variance
+#'
+#' Compute Variance for a numeric vector
+#'
+#' @param x a numeric vector
+#'
+#' @export
+#' @examples
+#' x <- rnorm(100,3,3)
+#' print(variance(x))
+
+
+
 variance <- function(x){
-  var <- (1/length(x))*sum((x-mean(x))^2)
+  var <- (1/(length(x)-1))*sum((x-mean(x))^2)
 }
+
 
